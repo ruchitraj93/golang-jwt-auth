bec341c19b6e4701a8eaa5bd4252f19e8ce47fbd
ruchitraj93 <66847489+ruchitraj93@users.noreply.github.com>
2022-11-13 19:03:44 +0530

    Add files via upload

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,11 @@
+package routes
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ruchit/golang-jwt-project/controllers"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controllers.Signup())
+	incomingRoutes.POST("users/login", controllers.Login())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,13 @@
+package routes
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ruchit/golang-jwt-project/controllers"
+	"github.com/ruchit/golang-jwt-project/middleware"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controllers.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+}
